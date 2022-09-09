37d76a36d2e59fbcc4f75d3740c54edf3f2d2058
alexpxrad <alexpxrad@gmail.com>
2022-09-08 20:19:12 -0400

    fixed order of array showing on Activity page

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,25 @@ import express from 'express'
 import cors from 'cors'
 import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
-//import functions from "firebase-functions"
+import functions from "firebase-functions"
+import { Message } from 'firebase-functions/v1/pubsub'
 
 
 const client = new MongoClient (process.env.MONGO_URI)
 const database = client.db('backend-app-node')
 const activities = database.collection('activity')
+
+
+
+
+
+// const timestamp =FieldValue.serverTimeStamp();
+// const newMessage = {...message, timestamp}   
+
+
+
+
+
 
 client.connect()
 console.log('Mongo connected')
@@ -20,19 +33,19 @@ app.listen(4040, () => console.log('api listening on port 4040'))
 
 
 app.get('/activity', async (req, res) => {
-   const allActivities = await activities.find().toArray()
+   const allActivities = await (await (await activities.find().toArray()).reverse())
    res.send(allActivities)
 })
 
 app.post('/activity/postactivity', async (req, res) => {
     
     await activities.insertOne(req.body )
-    res.send('Item was added to Mongo')
+    res.send({'Item was added to Mongo': true})
 })
 
 app.delete('/activity/deleteactivity', async (req, res) => {
     await activities.findOneAndDelete(req.query)
-    res.send('Item deleted')
+    res.send({'Item deleted': true})
 })
 
 
@@ -45,7 +58,8 @@ app.patch('/activity/updateactivity/:id', async (req, res) => {
     res.send(result.value)
 })
 
-//export const api = functions.https.onRequest(app)
+
+export const api = functions.https.onRequest(app)
 
 
 
